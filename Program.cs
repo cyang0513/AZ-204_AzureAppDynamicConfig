45d32c2d99e9bd5d03783111527e0c033bf67699
Chengkai Yang <yangchengkai@gmail.com>
2021-02-27 11:55:24 +0100

    sp for KV access

--- Program.cs
+++ Program.cs
@@ -37,11 +37,13 @@ namespace AzureAppDynamicConfig
                                                                                                              });
 
                                                                                          //Config KV reference
-                                                                                         az.ConfigureKeyVault(kv =>
-                                                                                                              {
-                                                                                                                 kv.SetCredential(new AzureCliCredential());
-                                                                                                              }
-                                                                                         );
+                                                                                         //Setup service principle to access key vault
+                                                                                         Environment.SetEnvironmentVariable("AZURE_CLIENT_ID ", "971c306c-8ea5-4247-8a07-7732facc8d58");
+                                                                                         Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "UQF-z_kFJRr~lCAWrwHslg0f1Q75-4rvMw");
+                                                                                         Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "4e6f57dc-a3d9-4a0c-818b-a7c1bb2b79f6");
+                                                                                         az.ConfigureKeyVault(kv => {
+                                                                                            kv.SetCredential(new DefaultAzureCredential());
+                                                                                         });
                                                                                       });
                                                                                    
                                                                                 });
